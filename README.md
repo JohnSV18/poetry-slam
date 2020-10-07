20bc88dbd8334816e209e93d51d880e4b8245e42
John Sag <johnsag@Johns-MBP-2.lan1>
2020-10-07 12:06:36 -0400

    added random function and backwards function as well

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # poetry-slam
- Welcome to my Poetry Slam porject.
+ Welcome to my Poetry Slam project.
  I will show you the same poem in various different ways. The poem I will be using is one by Rupi Kaur.
---
+++ poetry.py
@@ -0,0 +1,49 @@
+#importing random module to use in random function later on 
+import random
+
+#variable that holds the text file
+filename = "rupi.txt"
+
+#variable that has the text file open to read
+poem = open("rupi.txt", 'r')
+
+#this function is to read the lines
+def get_file_lines(filename):
+    file_lines = poem.readlines()
+    return file_lines
+    
+#variable lines_list holding the value you get when the function is called
+lines_list = get_file_lines(filename)
+
+#function that will print the lines backwards with the number of the line as well
+def lines_printed_backwards(lines_list):
+    #creating an empty list
+    numbered_lines = []
+
+    #for loop for every index in the length of the lines_list 
+    #you will add to the empty list with a space and then the number of the index
+    for i in range(len(lines_list)):
+        numbered_lines.append(str(i) + ' ' + lines_list[i])
+        #a variable that holds the list numbered_lines but in reverse
+    reverse_list = numbered_lines.reverse()
+
+    #for loop that prints out the lines of the numbered_lines list
+    for line in numbered_lines:
+        print(line.strip())
+
+#calling the lines_printed_backwards function
+lines_printed_backwards(lines_list)
+
+ #function that prints the lines in a random order
+def lines_printed_random(lines_list):
+    #a variable that holds the length of lines_list
+    list_len = len(lines_list)
+    #for loop that for every item in the range of the list_len
+    #you print lines_list at a random index
+    for i in range(list_len):
+        print(lines_list[random.randint(0,list_len-1)].strip())
+        
+    #calling the function to print the lines in a random order
+lines_printed_random(lines_list)
+
+
